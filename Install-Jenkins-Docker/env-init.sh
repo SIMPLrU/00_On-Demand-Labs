89395aa68731213716ad120cdd1ce89fc111f133
SIMPLrU <viet@simplru.com>
2018-06-29 16:46:28 +0300

    Add Step 2 Script

--- Install-Jenkins-Docker/env-init.sh
+++ Install-Jenkins-Docker/env-init.sh
@@ -1,2 +1,1 @@
-docker pull jenkins:latest    
-docker run -d -u root --name jenkins -p 8080:8080 -p 50000:50000 -v /root/jenkins_latest:/var/jenkins_home jenkins:latest
+docker exec -it jenkins cat /var/jenkins_home/secrets/initialAdminPassword
